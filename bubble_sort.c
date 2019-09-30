24e014e9fe56bcccc54c8f635b03aa3ba776aaa4
duikerddd <44222023+duikerddd@users.noreply.github.com>
2019-09-30 11:26:40 +0800

    Create bubble_sort.c

---
+++ bubble_sort.c
@@ -0,0 +1,34 @@
+#pragma once
+
+#include<stdio.h>
+
+void Swap(int *p1, int *p2)
+{
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+void BubbleSort(int *a, int n)
+{
+	//外层控制单趟排序
+	for (int end = n; end > 0; --end){
+		int exchange = 0;
+
+
+		//单趟排序
+		for (int i = 0; i < n - 1; ++i)
+		{
+			if (a[i]>a[i + 1])
+			{
+				Swap(&a[i], &a[i + 1]);
+				 exchange=1;
+			}
+		}
+		//如果没交换 说明有序
+		if (exchange == 0)
+		{
+			break;
+		}
+	}
+}
