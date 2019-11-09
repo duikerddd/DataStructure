863a45faca71357835471485ac1446bcb7adaedb
duikerddd <44222023+duikerddd@users.noreply.github.com>
2019-11-09 13:56:23 +0800

    Update and rename BinaryTree to BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -81,53 +81,7 @@ RESULT  CreateTree(TDataType preorder[], int size)
 	return result;
 }
 
-void Preorder(BNode *root)
-{
-	//终止条件
-	if (root == NULL)
-	{
-		return;
-	}
 
-	//根
-	printf("%d ", root->data);
-	//左子树,子问题用递归处理
-	Preorder(root->left);
-	//右子树,子问题用递归处理
-	Preorder(root->right);
-}
-
-void Inorder(BNode *root)
-{
-	//终止条件
-	if (root == NULL)
-	{
-		return;
-	}
-
-	//左子树,子问题用递归处理
-	Inorder(root->left);
-	//根
-	printf("%d ", root->data);
-	//右子树,子问题用递归处理
-	Inorder(root->right);
-}
-
-void Postorder(BNode *root)
-{
-	//终止条件
-	if (root == NULL)
-	{
-		return;
-	}
-
-	//左子树,子问题用递归处理
-	Postorder(root->left);
-	//右子树,子问题用递归处理
-	Postorder(root->right);
-	//根
-	printf("%d ", root->data);
-}
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
