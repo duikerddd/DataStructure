732f5302a4727748e5229d5d101fdd557a72384a
duikerddd <44222023+duikerddd@users.noreply.github.com>
2020-04-08 21:13:18 +0800

    Update binary_tree.h

--- binary_tree.h
+++ binary_tree.h
@@ -241,19 +241,7 @@ void PostorderLoop(BNode *root)
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`	
 
 
-void Mirror(BNode *root)
-{
-	if (root == NULL)
-	{
-		return;
-	}
-	BNode *t = root->left;
-	root->left = root->right;
-	root->right=t;
 
-	Mirror(root->left);
-	Mirror(root->right);
-}
 
 int IsBlance(BNode *root)
 {
