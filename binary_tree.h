e5f99e795f4ab2235ed6ec92f373a0b36a500ca3
duikerddd <44222023+duikerddd@users.noreply.github.com>
2020-04-13 19:14:02 +0800

    Update binary_tree.h

--- binary_tree.h
+++ binary_tree.h
@@ -124,47 +124,6 @@ void LevelOrder(BNode *root)
 	QueueDestroy(&queue);
 }
 
-//判断二叉树是否是完全二叉树
-
-//带空结点的层序遍历判断是否是完全二叉树
-int IsComplete(BNode *root)
-{
-	Queue queue;
-	QueueInit(&queue);
-	if (root == NULL)
-	{
-		return 1;
-	}
-
-	QueuePush(&queue, root);
-	while (!QueueEmpty(&queue))
-	{
-		BNode *front = QueueFront(&queue);
-		QueuePop(&queue);
-
-		if (front == NULL)
-		{
-			break;
-		}
-			QueuePush(&queue, front->left);
-			QueuePush(&queue, front->right);
-		
-	}
-	//发现空结点了
-	//查看队列中是否完全是NULL
-	while (!QueueEmpty(&queue))
-	{
-		BNode* node = QueueFront(&queue);
-		QueuePop(&queue);
-		if (node != NULL){
-			return 0;
-		}
-	}
-
-	QueueDestroy(&queue);
-	return 1;
-	
-}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
