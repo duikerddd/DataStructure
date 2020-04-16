084a7c6ec4d7a9a36c42627a66ef8ab9f59bf691
duikerddd <44222023+duikerddd@users.noreply.github.com>
2020-04-16 18:37:52 +0800

    Update binary_tree.h

--- binary_tree.h
+++ binary_tree.h
@@ -149,28 +149,5 @@ void PreorderLoop(BNode *root)
 	}
 }
 
-void InorderLoop(BNode *root)
-{
-	Stack stack;
-	StackInit(&stack);
-	BNode *node = root;
-	BNode*top;
-
-	while (node != NULL || !StackEmpty(&stack)){
-		while (node != NULL)
-		{	
-			StackPush(&stack, node); //压栈,压的是结点地址
-			node = node->left;
-		}
-
-		top = StackTop(&stack); StackPop(&stack);
-		printf("%d ", top->data);
-		node = top->right;
-	}
-}
-
-
-
-
 
 
